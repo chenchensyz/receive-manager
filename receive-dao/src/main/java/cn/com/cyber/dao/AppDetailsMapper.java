1230e671ff551a8364a9cc3e39d4ad92291a5ca5
chenchensyz <18603452952@163.com>
2019-03-23 12:46:00 +0800

    删除文件

--- receive-dao/src/main/java/cn/com/cyber/dao/AppDetailsMapper.java
+++
@@ -1,7 +0,0 @@
-package cn.com.cyber.dao;
-
-import cn.com.cyber.common.BaseDao;
-import cn.com.cyber.model.AppDetails;
-
-public interface AppDetailsMapper extends BaseDao<AppDetails> {
-}
--- receive-dao/src/main/java/cn/com/cyber/dao/logInvokInfoMapper.java
+++
@@ -1,7 +0,0 @@
-package cn.com.cyber.dao;
-
-import cn.com.cyber.common.BaseDao;
-import cn.com.cyber.model.logInvokInfo;
-
-public interface logInvokInfoMapper extends BaseDao<logInvokInfo> {
-}
